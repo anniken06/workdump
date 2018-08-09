574caca99cc09d4917412ced4f32792b2b069654
John Paul Guzman <JohnPaul.Guzman@infor.com>
2018-08-09 11:49:25 +0800

    esasd

--- python shenanigans/automate_requests.py
+++ python shenanigans/automate_requests.py
@@ -1,30 +1,112 @@
 import json
+import os
 import shlex
 import subprocess
+import time
 
 
-def run_subprocess_and_get_results(command):
+def run_subprocess_and_get_results(command, max_length=256, timeout=1):
     args = shlex.split(command)
     process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (out, err, rc) = *process.communicate(), process.returncode
-    print("Running: ", command)
-    print("Results: ", (out, err, rc))
-    return {"command": command, "stdout": out, "stderr": err, "returncode": rc}
+    #time.sleep(timeout)
+    print(">> Running: ", str(command)[ :max_length])
+    print(">> Results: ", str((out, err, rc))[ :max_length])
+    return {'command': command, 'stdout': out, 'stderr': err, 'returncode': rc}
 
 
-def write_to_file(filename, bytestring):
-    with open(filename, 'wb') as out_file:
-        out_file.write(bytestring)
+class CurlGenerator:
+    def __init__(self, curl_template, replace_dict={"": [""]}, handle_responses=lambda responses: None):
+        ## Copy from Chrome > F12 > Network > particular request > Right-click > Copy > Copy as cURL (bash)
+        self.curl_template = curl_template
+        self.replace_dict = replace_dict
+        self.handle_responses = handle_responses
+
+    def generate_curls(self):
+        curls = [self.curl_template]
+        for keyword, replacements in self.replace_dict.items():
+            curls = [curl.replace(keyword, replacement) for curl in curls for replacement in replacements]
+        return curls
+
+    def generate_responses(self, curls):
+        responses = []
+        for curl in curls:
+            responses += [run_subprocess_and_get_results(curl)]
+        return responses
+
+    def process_responses(self):
+        curls = self.generate_curls()
+        responses = self.generate_responses(curls)
+        return self.handle_responses(responses)
 
 
-## Copy from Chrome > F12 > Network > particular request > Right-click > Copy > Copy as cURL (bash)
-curl_get = "curl 'https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://manila1.cpaas.awsiondev.infor.com:18010/coleman/' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36' --compressed --insecure"
-curl_save = "curl 'https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest/eedc621d-4102-40ee-8429-2372165b43f3?render=true' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://manila1.cpaas.awsiondev.infor.com:18010/coleman/' -H 'Cookie: XSRF-TOKEN=70d04811-95ac-4d7c-bd01-94ed7d2b642d; JSESSIONID=CFD726F8A8E60B15E038F493990EB8F5' -H 'Connection: keep-alive' --compressed --insecure"
-curl_delete = "curl 'https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest/<DS_ID>' -X DELETE -H 'Origin: https://manila1.cpaas.awsiondev.infor.com:18010' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Referer: https://manila1.cpaas.awsiondev.infor.com:18010/coleman/' -H 'Cookie: XSRF-TOKEN=70d04811-95ac-4d7c-bd01-94ed7d2b642d; JSESSIONID=CFD726F8A8E60B15E038F493990EB8F5' -H 'Connection: keep-alive' --compressed --insecure"
+if __name__ == '__main__':
+    backup_directory = "backups"
 
-results_get = run_subprocess_and_get_results(curl_get)
-quests = json.loads(results_get["stdout"].decode('utf-8'))
-for quest in quests:
-    results_save = run_subprocess_and_get_results(curl_save.replace("<DS_ID>", quest["id"]))
-    write_to_file("backup_{}_{}.json".format(quest["name"], quest["id"]), results_save["stdout"])
-    results_delete = run_subprocess_and_get_results(curl_delete.replace("<DS_ID>", quest["id"]))
+    # # #  Query quests  # # # 
+    get_template = "curl -v '<API_URL>' -H 'Accept: application/json, text/plain, */*' --insecure"
+    get_replace_dict = {
+        "<API_URL>": ["https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest"],
+    }
+    def get_handler(responses):
+        print("Processing responses from: get curls.")
+        try:
+            quests = json.loads(responses[0]['stdout'].decode('utf-8'))
+            print("Finished looking for {} quests: {}.".format(len(quests), [quest['name'] for quest in quests]))
+        except Exception as e:
+            print(e)
+            print(responses[0]['stderr'].decode('utf-8'))
+        return quests
+    get_generator = CurlGenerator(curl_template=get_template, replace_dict=get_replace_dict, handle_responses=get_handler)
+    quests = get_generator.process_responses()
+
+
+    # # #  Save queried quests  # # # 
+    save_template = "curl -v '<API_URL>/<DS_ID>?render=true' -H 'Accept: application/json, text/plain, */*' --insecure"
+    save_replace_dict = {
+        "<API_URL>": ["https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest"],
+        "<DS_ID>": [quest['id'] for quest in quests],
+    }
+    def save_handler(responses):
+        print("Processing responses from: save curls.")
+        if not os.path.exists(backup_directory): os.makedirs(backup_directory)
+        for result in responses:
+            try:
+                saved_quest =  json.loads(result['stdout'].decode('utf-8'))['quest']
+                backup_file = os.path.join(backup_directory, "backup_{}_{}.json".format(saved_quest["name"], saved_quest["id"]))
+                with open(backup_file, 'wb') as out_file:
+                    out_file.write(result['stdout'])
+                print("Successfully saved: {}".format(backup_file))
+            except Exception as e:
+                print(e)
+                print(result['stderr'].decode('utf-8'))
+        return "Finished saving backups to: /{}/.".format(backup_directory)
+    save_generator = CurlGenerator(curl_template=save_template, replace_dict=save_replace_dict, handle_responses=save_handler)
+    print("Backup status: {}".format(save_generator.process_responses()))
+
+
+    # # #  Delete queried quests  # # # 
+    delete_template = "curl -v '<API_URL>/<DS_ID>' -X DELETE -H 'Accept: application/json, text/plain, */*' --insecure"
+    delete_replace_dict = {
+        "<API_URL>": ["https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest"],
+        "<DS_ID>": [quest['id'] for quest in quests],
+    }
+    def delete_handler(responses):
+        print("Processing responses from: delete curls.")
+        return "Finished deleting {} quests.".format(len(responses))
+    delete_generator = CurlGenerator(curl_template=delete_template, replace_dict=delete_replace_dict, handle_responses=delete_handler)
+    print("Delete status: {}".format(delete_generator.process_responses()))
+
+
+    print("Done!")
+
+
+""" SAMPLE
+updated_quest = quest
+updated_quest . add/remove col
+
+update quest template = "... <API>/<ID> ... <NEW_CONTENTS>"
+{"<NEW CONENTS>": updated_quest}
+
+process_responses()
+"""
