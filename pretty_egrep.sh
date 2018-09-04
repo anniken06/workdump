c8e99e321edd1c8e40791205d0e7e9b4d7b575aa
John Paul Guzman <JohnPaul.Guzman@infor.com>
2018-09-04 15:57:15 +0800

    egrpe

--- pretty_egrep.sh
+++ pretty_egrep.sh
@@ -1,1 +1,1 @@
-reset && ls -t | grep -E ".out$" | head -n 1 | xargs -I{} tail -f -n +0 {} | nl | grep -v -E "getDataset()|getQuest()" | grep -E "$1" | cut -c 1-512 | xargs -d '\n' -I{} printf "{}\n\n"
+reset && ls -t | grep -E ".out$" | head -n 1 | xargs -I{} tail -f -n +0 {} | nl | grep -v -E "getDataset()|getQuest()" | grep -E "$1" | cut -c 1-1024 | xargs -d '\n' -I{} printf "{}\n\n"
