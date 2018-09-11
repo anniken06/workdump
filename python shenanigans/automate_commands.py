5490dee6ab14efe0de7530be3b6f9de2ee2ab4f9
John Paul Guzman <JohnPaul.Guzman@infor.com>
2018-09-11 14:05:44 +0800

    123

--- python shenanigans/automate_commands.py
+++ python shenanigans/automate_commands.py
@@ -5,46 +5,41 @@ import subprocess
 import time
 
 
-def run_subprocess_and_get_results(command, max_length=1024, timeout=1):
-    print(">> Running: ", str(command)[ :max_length])
-    args = shlex.split(command) ## while err??
-    process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (out, err, rc) = *process.communicate(), process.returncode
-    time.sleep(timeout)
-    print(">> Results: ", str((out, err, rc))[ :max_length])
-    if rc != 0:
-        print(">> Command failed!")
-        if input("Retry command (y)?: ") == 'y':
-            return run_subprocess_and_get_results(command)
-    return {'command': command, 'stdout': out, 'stderr': err, 'returncode': rc}
-
-
-class CurlGenerator:
-    @staticmethod  # allows the method to be called by an instance, unnecessary for now
-    def default_response_handler(responses):
-        print("Handler not implemented. Simply printing responses:\n\n", "\n\n".join(map(lambda response: str(response), responses)))
-        return responses
-
-    def __init__(self, curl_template, replace_dict={"": [""]}, handle_responses=default_response_handler):
-        self.curl_template = curl_template
+class CommandGenerator:
+    def __init__(self, command_template, replace_dict={"": [""]}, handle_responses=lambda responses: responses):
+        self.command_template = command_template
         self.replace_dict = replace_dict
         self.handle_responses = handle_responses
 
-    def generate_curls(self):
-        curls = [self.curl_template]
+    def generate_commands(self):
+        commands = [self.command_template]
         for keyword, replacements in self.replace_dict.items():
-            curls = [curl.replace(keyword, replacement) for curl in curls for replacement in replacements]
-        return curls
+            commands = [command.replace(keyword, replacement) for command in commands for replacement in replacements]
+        return commands
 
-    def generate_responses(self, curls):
+    def run_subprocess_and_get_results(self,command, max_length=1024, timeout=1):
+        print(">> Running: ", str(command)[ :max_length])
+        args = shlex.split(command) ## while err??
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (out, err, rc) = *process.communicate(), process.returncode
+        time.sleep(timeout)
+        print(">> Results: ", str((out, err, rc))[ :max_length])
+        if rc != 0:
+            print(">> Command failed!")
+            if auto_y or input("Retry command (y)?: ") == 'y':
+                return self.run_subprocess_and_get_results(command)
+        return {'command': command, 'stdout': out, 'stderr': err, 'returncode': rc}
+
+    def generate_responses(self, commands):
         responses = []
-        for curl in curls:
-            responses += [run_subprocess_and_get_results(curl)]
+        for command in commands:
+            responses += [self.run_subprocess_and_get_results(command)]
         return responses
 
     def process_responses(self):
-        curls = self.generate_curls()
-        responses = self.generate_responses(curls)
+        commands = self.generate_commands()
+        responses = self.generate_responses(commands)
+        print("Printing responses:\n\n", "\n\n".join(map(lambda response: str(response), responses)))
         return self.handle_responses(responses)
 
 
@@ -54,7 +49,7 @@ def get_quests():
         "<API_URL>": ["https://manila1.cpaas.awsiondev.infor.com:18010/coleman/api/quest"],
     }
     def get_handler(responses):
-        print("Processing responses from: get curls.")
+        print("Processing responses from: get commands.")
         try:
             quests = json.loads(responses[0]['stdout'].decode('utf-8'))
             print("Finished looking for {} quests: {}.".format(len(quests), [quest['name'] for quest in quests]))
@@ -63,7 +58,7 @@ def get_quests():
             print(responses[0]['stderr'].decode('utf-8'))
         return quests
     if input("Run get tasks (y)? ") == 'y':
-        get_generator = CurlGenerator(curl_template=get_template, replace_dict=get_replace_dict, handle_responses=get_handler)
+        get_generator = CommandGenerator(command_template=get_template, replace_dict=get_replace_dict, handle_responses=get_handler)
         quests = get_generator.process_responses()
         return quests
 
@@ -74,7 +69,7 @@ def save_quests(quests):
         "<DS_ID>": [quest['id'] for quest in quests],
     }
     def save_handler(responses):
-        print("Processing responses from: save curls.")
+        print("Processing responses from: save commands.")
         if not os.path.exists(backup_directory): os.makedirs(backup_directory)
         for result in responses:
             try:
@@ -88,7 +83,7 @@ def save_quests(quests):
                 print(result['stderr'].decode('utf-8'))
         return "Finished saving backups to: /{}/.".format(backup_directory)
     if input("Run save tasks (y)? ") == 'y':
-        save_generator = CurlGenerator(curl_template=save_template, replace_dict=save_replace_dict, handle_responses=save_handler)
+        save_generator = CommandGenerator(command_template=save_template, replace_dict=save_replace_dict, handle_responses=save_handler)
         print("Backup status: {}".format(save_generator.process_responses()))
 
 def delete_quests(quests):
@@ -99,7 +94,7 @@ def delete_quests(quests):
         #"<DS_ID>": [quest['id'] for quest in quests if len(quest['activities']) < 2],
     }
     if input("Run delete tasks (y)? ") == 'y':
-        delete_generator = CurlGenerator(curl_template=delete_template, replace_dict=delete_replace_dict)
+        delete_generator = CommandGenerator(command_template=delete_template, replace_dict=delete_replace_dict)
         print("Delete status: {}".format(delete_generator.process_responses()))
 
 def update_quests(quests):
@@ -109,11 +104,11 @@ def update_quests(quests):
         "<QUEST_JSON>": [json.dumps({"quest": quest}) for quest in quests],
     }
     if input("Run update tasks (y)? ") == 'y':
-        update_generator = CurlGenerator(curl_template=update_template, replace_dict=update_replace_dict)
+        update_generator = CommandGenerator(command_template=update_template, replace_dict=update_replace_dict)
         responses = update_generator.process_responses()
         print("Update status: {}".format(responses))
 
-def scan_dynamo(region_name, table_name):
+def dynamo_scan_keys(region_name, table_name):
     scan_template = """aws dynamodb scan \
 --region '<REGION_NAME>' \
 --table-name '<TABLE_NAME>' \
@@ -126,12 +121,12 @@ def scan_dynamo(region_name, table_name):
     def scan_handler(responses):
         load_scan_result = json.loads(responses[0]['stdout'].decode('utf-8'))
         return load_scan_result['Items']
-    if input("Run scan tasks (y)? ") == 'y':
-        scan_generator = CurlGenerator(curl_template=scan_template, replace_dict=scan_replace_dict, handle_responses=scan_handler)
+    if auto_y or input("Run scan tasks (y)? ") == 'y':
+        scan_generator = CommandGenerator(command_template=scan_template, replace_dict=scan_replace_dict, handle_responses=scan_handler)
         keys = scan_generator.process_responses()
         return keys
 
-def add_string_field(region_name, table_name, keys, field_name, dummy_string):
+def dynamo_add_string_field(region_name, table_name, keys, field_name, dummy_string):
     add_template = """
 aws dynamodb update-item \
 --region '<REGION_NAME>' \
@@ -147,13 +142,13 @@ aws dynamodb update-item \
         "<FIELD_NAME>": [field_name],
         "<DUMMY_STRING>": [dummy_string],
     }
-    if input("Run add tasks (y)? ") == 'y':
-        add_generator = CurlGenerator(curl_template=add_template, replace_dict=add_replace_dict)
+    if auto_y or input("Run add tasks (y)? ") == 'y':
+        add_generator = CommandGenerator(command_template=add_template, replace_dict=add_replace_dict)
         responses = add_generator.process_responses()
         print("Add status: {}".format(responses))
         return responses
 
-def delete_string_field(region_name, table_name, keys, field_name):
+def dynamo_delete_string_field(region_name, table_name, keys, field_name):
     delete_template = """
 aws dynamodb update-item \
 --region '<REGION_NAME>' \
@@ -163,12 +158,12 @@ aws dynamodb update-item \
     """
     delete_replace_dict = {
         "<REGION_NAME>": [region_name],
-        "<TABLE_NAME>": [],
+        "<TABLE_NAME>": [table_name],
         "<KEY>": [json.dumps(key) for key in keys],
         "<FIELD_NAME>": [field_name],
     }
     if input("Run delete tasks (y)? ") == 'y':
-        delete_generator = CurlGenerator(curl_template=delete_template, replace_dict=delete_replace_dict)
+        delete_generator = CommandGenerator(command_template=delete_template, replace_dict=delete_replace_dict)
         responses = delete_generator.process_responses()
         print("Delete status: {}".format(responses))
         return responses
@@ -182,13 +177,24 @@ if __name__ == '__main__':
     ##########################
     ###  Modify execution  ###
     ##########################
-
+    auto_y = True
     region_name = "eu-west-1"
-    table_name = "coleman_dataset"
+    table_name = "coleman_dataset_manila1"
 
-    keys = scan_dynamo(region_name, table_name)
-    add_responses = add_string_field(region_name, table_name, keys, "newField", "dummy_string")
-    delete_responses = delete_string_field(region_name, table_name, keys, "newField")
+    keys = dynamo_scan_keys(region_name, table_name)
+
+    new_fields = {
+        "createdBy": "Jay Ryan Ramos",
+        "createdOn": "2018-09-07T03:52:55.152+0000",
+        "lastUpdatedBy": "Jay Ryan Ramos",
+        "lastUpdatedOn": "2018-09-07T03:52:55.152+0000",
+    }
+
+    add_responses = []
+    for field, value in new_fields.items():
+        add_responses += [dynamo_add_string_field(region_name, table_name, keys, field, value)]
+
+    #for delete_field in new_fields.keys(): dynamo_delete_string_field(region_name, table_name, keys, delete_field)
 
     ##########################
     ###  Modify execution  ###
